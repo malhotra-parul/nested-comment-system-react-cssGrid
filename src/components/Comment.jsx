ac480626113a223da5264f61c6235c50cdc9d70e
Parul Malhotra <parulm.business@gmail.com>
2020-09-16 20:08:32 +0530

    Add edit functionality

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,6 +5,7 @@ const Comment = ({ comment }) => {
   const [showInput, setShowInput] = useState(false);
   const [reply, setReply] = useState("");
   const [child, setChild] = useState(comment.children);
+  const [edit, setEdit] = useState(false);
 
   const nestedComments = (child || []).map((comment) => (
     <Comment comment={comment} key={comment.id} />
@@ -19,6 +20,7 @@ const Comment = ({ comment }) => {
   };
 
   const changeValue = (e) => {
+    console.log("change value");
     setReply(e.target.value);
   };
 
@@ -32,11 +34,33 @@ const Comment = ({ comment }) => {
     setChild([...child, newComment]);
   };
 
+  const editComment = (e) => {
+    setEdit(!edit);
+    setReply(comment.content);
+  };
+
+  const save = (e) => {
+    console.log(reply);
+    comment.content = reply;
+    setEdit(false);
+  };
+
   return (
     <div className="comment-box">
       <div className="author">{comment.postedBy}: </div>
       <div className="buttons">
-        <button className="edit">Edit</button>
+        <button className="edit" onClick={editComment}>
+          Edit
+        </button>
+        {edit && (
+          <>
+            {" "}
+            <textarea rows="5" value={reply} onChange={changeValue}>
+              {reply}
+            </textarea>
+            <button onClick={save}>Save</button>
+          </>
+        )}
         <button className="delete">Delete</button>
       </div>
       <div className="content">{comment.content}</div>
